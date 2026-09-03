4cc48c8505424df4ea2f5d263a82b3f9e8869145
geg971509-wq <geg971509@gmail.com>
2026-09-03 20:38:40 +0900

    Validate forward and reverse branch target domains

--- internal/runtime/templates/android/arm64/vm_handlers/h_branch.h
+++ internal/runtime/templates/android/arm64/vm_handlers/h_branch.h
@@ -13,8 +13,13 @@
       (vm)->pc += (size);                                                      \
   } while (0)
 
+/* Forward targets identify an opcode byte in [0, bc_len). Reverse targets
+ * identify the byte immediately after an instruction and therefore live in
+ * (0, bc_len]. */
 static inline int vm_branch_target_valid(vm_ctx_t *vm, u32 target) {
-  if (target < vm->bc_len)
+  int valid = vm->reverse ? (target != 0 && target <= vm->bc_len)
+                          : (target < vm->bc_len);
+  if (valid)
     return 1;
   vm_fault_set(vm, VM_FAULT_CONTROL);
   return 0;
@@ -29,113 +34,79 @@ static inline u32 h_jmp(vm_ctx_t *vm) {
 
 static inline u32 h_je(vm_ctx_t *vm) {
   u32 target = rd32(&vm->bc[vm->pc + 1]);
-  if (!vm_branch_target_valid(vm, target))
-    return 0;
-  if (vm->FL & FL_Z)
-    vm->pc = target;
-  else
-    BRANCH_FALLTHROUGH(vm, 5);
+  if (!vm_branch_target_valid(vm, target)) return 0;
+  if (vm->FL & FL_Z) vm->pc = target; else BRANCH_FALLTHROUGH(vm, 5);
   return 0;
 }
 
 static inline u32 h_jne(vm_ctx_t *vm) {
   u32 target = rd32(&vm->bc[vm->pc + 1]);
-  if (!vm_branch_target_valid(vm, target))
-    return 0;
-  if (!(vm->FL & FL_Z))
-    vm->pc = target;
-  else
-    BRANCH_FALLTHROUGH(vm, 5);
+  if (!vm_branch_target_valid(vm, target)) return 0;
+  if (!(vm->FL & FL_Z)) vm->pc = target; else BRANCH_FALLTHROUGH(vm, 5);
   return 0;
 }
 
 static inline u32 h_jl(vm_ctx_t *vm) {
   u32 target = rd32(&vm->bc[vm->pc + 1]);
-  if (!vm_branch_target_valid(vm, target))
-    return 0;
-  if (!!(vm->FL & FL_N) != !!(vm->FL & FL_V))
-    vm->pc = target;
-  else
-    BRANCH_FALLTHROUGH(vm, 5);
+  if (!vm_branch_target_valid(vm, target)) return 0;
+  if (!!(vm->FL & FL_N) != !!(vm->FL & FL_V)) vm->pc = target;
+  else BRANCH_FALLTHROUGH(vm, 5);
   return 0;
 }
 
 static inline u32 h_jge(vm_ctx_t *vm) {
   u32 target = rd32(&vm->bc[vm->pc + 1]);
-  if (!vm_branch_target_valid(vm, target))
-    return 0;
-  if (!!(vm->FL & FL_N) == !!(vm->FL & FL_V))
-    vm->pc = target;
-  else
-    BRANCH_FALLTHROUGH(vm, 5);
+  if (!vm_branch_target_valid(vm, target)) return 0;
+  if (!!(vm->FL & FL_N) == !!(vm->FL & FL_V)) vm->pc = target;
+  else BRANCH_FALLTHROUGH(vm, 5);
   return 0;
 }
 
 static inline u32 h_jgt(vm_ctx_t *vm) {
   u32 target = rd32(&vm->bc[vm->pc + 1]);
-  if (!vm_branch_target_valid(vm, target))
-    return 0;
-  if (!(vm->FL & FL_Z) &&
-      (!!(vm->FL & FL_N) == !!(vm->FL & FL_V)))
+  if (!vm_branch_target_valid(vm, target)) return 0;
+  if (!(vm->FL & FL_Z) && (!!(vm->FL & FL_N) == !!(vm->FL & FL_V)))
     vm->pc = target;
-  else
-    BRANCH_FALLTHROUGH(vm, 5);
+  else BRANCH_FALLTHROUGH(vm, 5);
   return 0;
 }
 
 static inline u32 h_jle(vm_ctx_t *vm) {
   u32 target = rd32(&vm->bc[vm->pc + 1]);
-  if (!vm_branch_target_valid(vm, target))
-    return 0;
-  if ((vm->FL & FL_Z) ||
-      (!!(vm->FL & FL_N) != !!(vm->FL & FL_V)))
+  if (!vm_branch_target_valid(vm, target)) return 0;
+  if ((vm->FL & FL_Z) || (!!(vm->FL & FL_N) != !!(vm->FL & FL_V)))
     vm->pc = target;
-  else
-    BRANCH_FALLTHROUGH(vm, 5);
+  else BRANCH_FALLTHROUGH(vm, 5);
   return 0;
 }
 
 static inline u32 h_jb(vm_ctx_t *vm) {
   u32 target = rd32(&vm->bc[vm->pc + 1]);
-  if (!vm_branch_target_valid(vm, target))
-    return 0;
-  if (!(vm->FL & FL_C))
-    vm->pc = target;
-  else
-    BRANCH_FALLTHROUGH(vm, 5);
+  if (!vm_branch_target_valid(vm, target)) return 0;
+  if (!(vm->FL & FL_C)) vm->pc = target; else BRANCH_FALLTHROUGH(vm, 5);
   return 0;
 }
 
 static inline u32 h_jae(vm_ctx_t *vm) {
   u32 target = rd32(&vm->bc[vm->pc + 1]);
-  if (!vm_branch_target_valid(vm, target))
-    return 0;
-  if (vm->FL & FL_C)
-    vm->pc = target;
-  else
-    BRANCH_FALLTHROUGH(vm, 5);
+  if (!vm_branch_target_valid(vm, target)) return 0;
+  if (vm->FL & FL_C) vm->pc = target; else BRANCH_FALLTHROUGH(vm, 5);
   return 0;
 }
 
 static inline u32 h_jbe(vm_ctx_t *vm) {
   u32 target = rd32(&vm->bc[vm->pc + 1]);
-  if (!vm_branch_target_valid(vm, target))
-    return 0;
-  if (!(vm->FL & FL_C) || (vm->FL & FL_Z))
-    vm->pc = target;
-  else
-    BRANCH_FALLTHROUGH(vm, 5);
+  if (!vm_branch_target_valid(vm, target)) return 0;
+  if (!(vm->FL & FL_C) || (vm->FL & FL_Z)) vm->pc = target;
+  else BRANCH_FALLTHROUGH(vm, 5);
   return 0;
 }
 
 static inline u32 h_ja(vm_ctx_t *vm) {
   u32 target = rd32(&vm->bc[vm->pc + 1]);
-  if (!vm_branch_target_valid(vm, target))
-    return 0;
-  if ((vm->FL & FL_C) && !(vm->FL & FL_Z))
-    vm->pc = target;
-  else
-    BRANCH_FALLTHROUGH(vm, 5);
+  if (!vm_branch_target_valid(vm, target)) return 0;
+  if ((vm->FL & FL_C) && !(vm->FL & FL_Z)) vm->pc = target;
+  else BRANCH_FALLTHROUGH(vm, 5);
   return 0;
 }
 
@@ -143,12 +114,9 @@ static inline u32 h_tbz(vm_ctx_t *vm) {
   u8 reg = vm->bc[vm->pc + 1];
   u8 bit = vm->bc[vm->pc + 2];
   u32 target = rd32(&vm->bc[vm->pc + 3]);
-  if (!vm_branch_target_valid(vm, target))
-    return 0;
-  if (!(vm->R[reg & 31] & ((u64)1 << (bit & 63))))
-    vm->pc = target;
-  else
-    BRANCH_FALLTHROUGH(vm, 7);
+  if (!vm_branch_target_valid(vm, target)) return 0;
+  if (!(vm->R[reg & 31] & ((u64)1 << (bit & 63)))) vm->pc = target;
+  else BRANCH_FALLTHROUGH(vm, 7);
   return 0;
 }
 
@@ -156,24 +124,18 @@ static inline u32 h_tbnz(vm_ctx_t *vm) {
   u8 reg = vm->bc[vm->pc + 1];
   u8 bit = vm->bc[vm->pc + 2];
   u32 target = rd32(&vm->bc[vm->pc + 3]);
-  if (!vm_branch_target_valid(vm, target))
-    return 0;
-  if (vm->R[reg & 31] & ((u64)1 << (bit & 63)))
-    vm->pc = target;
-  else
-    BRANCH_FALLTHROUGH(vm, 7);
+  if (!vm_branch_target_valid(vm, target)) return 0;
+  if (vm->R[reg & 31] & ((u64)1 << (bit & 63))) vm->pc = target;
+  else BRANCH_FALLTHROUGH(vm, 7);
   return 0;
 }
 
 static inline u32 h_jcond(vm_ctx_t *vm) {
   u8 cond = vm->bc[vm->pc + 1] & 0xFu;
   u32 target = rd32(&vm->bc[vm->pc + 2]);
-  if (!vm_branch_target_valid(vm, target))
-    return 0;
-  if (vm_cond_holds(vm, cond))
-    vm->pc = target;
-  else
-    BRANCH_FALLTHROUGH(vm, 6);
+  if (!vm_branch_target_valid(vm, target)) return 0;
+  if (vm_cond_holds(vm, cond)) vm->pc = target;
+  else BRANCH_FALLTHROUGH(vm, 6);
   return 0;
 }
 
@@ -181,14 +143,9 @@ static inline u32 h_cbz(vm_ctx_t *vm) {
   u8 reg = vm->bc[vm->pc + 1];
   u32 target = rd32(&vm->bc[vm->pc + 2]);
   u64 value = vm->R[reg & 31];
-  if (!vm_branch_target_valid(vm, target))
-    return 0;
-  if (!(reg & 0x80u))
-    value &= 0xFFFFFFFFULL;
-  if (value == 0)
-    vm->pc = target;
-  else
-    BRANCH_FALLTHROUGH(vm, 6);
+  if (!vm_branch_target_valid(vm, target)) return 0;
+  if (!(reg & 0x80u)) value &= 0xFFFFFFFFULL;
+  if (value == 0) vm->pc = target; else BRANCH_FALLTHROUGH(vm, 6);
   return 0;
 }
 
@@ -196,14 +153,9 @@ static inline u32 h_cbnz(vm_ctx_t *vm) {
   u8 reg = vm->bc[vm->pc + 1];
   u32 target = rd32(&vm->bc[vm->pc + 2]);
   u64 value = vm->R[reg & 31];
-  if (!vm_branch_target_valid(vm, target))
-    return 0;
-  if (!(reg & 0x80u))
-    value &= 0xFFFFFFFFULL;
-  if (value != 0)
-    vm->pc = target;
-  else
-    BRANCH_FALLTHROUGH(vm, 6);
+  if (!vm_branch_target_valid(vm, target)) return 0;
+  if (!(reg & 0x80u)) value &= 0xFFFFFFFFULL;
+  if (value != 0) vm->pc = target; else BRANCH_FALLTHROUGH(vm, 6);
   return 0;
 }
 
